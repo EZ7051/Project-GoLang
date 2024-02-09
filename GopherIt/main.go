70a0f2b4fc14c12eb9bf3426d7a4211fb5aa8a23
Ejaz Ahamed Shaik <35006207+EZ7051@users.noreply.github.com>
2024-02-08 21:47:32 -0500

    Create main.go

---
+++ GopherIt/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import t "time"
+
+func main(){
+
+  fmt.Println("  __      _")
+  fmt.Println("o'')}____//")
+  fmt.Println(" `_/      )")
+  fmt.Println(" (_(_/-(_/ ")
+
+  fmt.Println("Current time is ", t.Now())
+}
